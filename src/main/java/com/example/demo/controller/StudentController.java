ea62c58c0cd0924de038bf5c1f479add3b438a65
bala-amypo <bala@amypo.in>
2025-12-17 08:38:30 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,42 +1,23 @@
-package com.example.Springproject.Controller;
-
-import java.util.*;
+package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.Springproject.Entity.StudentEntity;
-import com.example.Springproject.Service.StudentService;
+import com.example.demo.entity.StudentEntity;
+import com.example.demo.service.StudentService;
+
 
 @RestController
 public class StudentController {
 
-
     @Autowired
     StudentService ser;
 
-    @PostMapping("/postStudents")
-    public StudentEntity addStudents(@RequestBody StudentEntity st){
-        return ser.addStudents(st);
-    }
-
-    @GetMapping("/getAllStudents")
-    public List<StudentEntity> getAllStudents(){
-        return ser.getAllStudents();
-    }
-    @GetMapping("/getStudent/{id}")
-    public StudentEntity getStudent(@PathVariable int id){
-        return ser.getStudent(id);
+    @PostMapping("/addStudents")
+    public StudentEntity addStudents(@RequestBody StudentEntity student){
+        return ser.addStudents(student);
     }
     
-    @DeleteMapping("/getStudent/{id}")
-    public StudentEntity deleteStudent(@PathVariable int id){
-        return ser.deleteStudent(id);
-    }
-
 }
--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,5 +1,34 @@
-package com.example.demo.service;
+package com.example.Springproject.Service;
 
-public class StudentService{
+import org.springframework.stereotype.Service;
 
-}
+
+import com.example.Springproject.Entity.StudentEntity;
+import java.util.*;
+
+
+@Service
+public class StudentService {
+    Map<Integer, StudentEntity> details = new HashMap<>();
+
+
+    public StudentEntity addStudents(StudentEntity st){
+        details.put(st.getId(),st);
+        return st;
+    }
+
+    public List<StudentEntity> getAllStudents() {
+        return new ArrayList<>(details.values()) ;
+    }
+
+    public StudentEntity getStudent(int id) {
+        return details.get(id);
+    }
+
+    public StudentEntity deleteStudent(int id) {
+        details.remove(id);
+        return details.get(id);
+    }
+
+    
+}   
