6ddf464cf5143544d56ea0dc9cb141394a1fc2c1
bala-amypo <bala@amypo.in>
2025-12-17 05:04:37 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -5,5 +5,55 @@ import jakarta.persistence.Entity;
 
 @Entity
 public class StudentEntity{
+    private Long id;
+    private string name;
+    private string email;
+    private string password;
+    private float cgpa;
 
+    public StudentEntity(int id,String name,String email, double cgpa){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.cgpa = cgpa;
+    }
+
+    public StudentEntity(){
+
+    }
+
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCgpa(double cgpa) {
+        this.cgpa = cgpa;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public double getCgpa() {
+        return cgpa;
+    }
+
+    
 }
